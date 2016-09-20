e50d1fb8c5bbc6bdb846b342a322d009d14be57a
root <root@tim.sh.intel.com>
2016-09-20 15:50:38 +0800

    RMContainerRequest askToAllocate5

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/rm/RMContainerRequestor.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/rm/RMContainerRequestor.java
@@ -207,9 +207,12 @@ public abstract class RMContainerRequestor extends RMCommunicator {
     }
     ArrayList<ResourceRequest> askToAllocate = new ArrayList<ResourceRequest>();
     if(askList.size() == 5) {
-      askToAllocate.add(askList.get(0));
+      //askToAllocate.add(askList.get(0));
       askToAllocate.add(askList.get(1));
       askToAllocate.add(askList.get(askList.size() - 1));
+    }
+    else {
+      askToAllocate = askList;
     }
     for(ResourceRequest resourceRequest : askToAllocate) {
       LOG.info("@makeRemoteRequest askToAllocate : " + resourceRequest.getPriority()+
